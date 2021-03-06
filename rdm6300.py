b6f620bd20070cf26a28e385115e28913e05181c
Stefan Golinschi <stefan.golinschi@gmail.com>
2021-03-06 07:48:19 +0200

    small code refactor

--- rdm6300.py
+++ rdm6300.py
@@ -5,16 +5,20 @@ import serial
 #  =============================================
 # params: 96008n1
 
-class RDM6300:
-    SERIAL_PORT = ''
-    BAUDRATE = 9600
+from enum import Enum
 
-    RDM_START = 2
-    RDM_END = 3
-    RDM_DATA_SZ = 20
 
-    def __init__(self, serial_port):
-        self.SERIAL_PORT = serial_port
+class RdmHdr(Enum):
+    start_offset = 2
+    end_offset = 3
+    data_size = 20
+
+
+class Rdm6300:
+    baud_rate = 9600
+
+    def __init__(self, serial_port: str):
+        self.serial_port = serial_port
 
     @staticmethod
     def __verify_checksum(data, checksum):
@@ -41,7 +45,7 @@ class RDM6300:
         tag_string = ''
         byte_read = serial_connection.read()
 
-        if int(ord(byte_read)) != self.RDM_START:
+        if int(ord(byte_read)) != RdmHdr.start_offset:
             return False
 
         expected_len = 12
@@ -50,11 +54,11 @@ class RDM6300:
             expected_len -= 1
             byte_read = serial_connection.read()
 
-            if int(ord(byte_read)) == self.RDM_START:
+            if int(ord(byte_read)) == RdmHdr.start_offset:
                 expected_len = 12
                 continue
 
-            if ord(byte_read) != self.RDM_END:
+            if ord(byte_read) != RdmHdr.end_offset:
                 tag_string += chr(ord(byte_read))
                 continue
             break
@@ -70,7 +74,7 @@ class RDM6300:
     def do_work(self):
         serial_connection = ''
         try:
-            serial_connection = serial.Serial(self.SERIAL_PORT, baudrate=self.BAUDRATE)
+            serial_connection = serial.Serial(self.serial_port, baudrate=self.baud_rate)
 
             while True:
                 data = self.__read_sequence(serial_connection)
@@ -83,9 +87,11 @@ class RDM6300:
                 serial_connection.flushInput()
 
         except KeyboardInterrupt:
-            print("\nKilled. Serial port was safely closed.")
+            print('Killed. Serial port was safely closed.')
             serial_connection.close()
 
-if __name__ == "__main__":
-    rdm6300_reader = RDM6300('/dev/ttyS0')
+
+if __name__ == '__main__':
+    serial_port = '/dev/ttyS0'
+    rdm6300_reader = Rdm6300(serial_port)
     rdm6300_reader.do_work()
